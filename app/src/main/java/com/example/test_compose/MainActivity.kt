119ff7034f7f562d99d7cfad8642cbe89826bf15
AmrNasser <amr931111@gmail.com>
2024-05-14 21:16:07 +0300

    test State and remember

--- app/src/main/java/com/example/test_compose/MainActivity.kt
+++ app/src/main/java/com/example/test_compose/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.BoxScope
 import androidx.compose.foundation.layout.Row
@@ -12,6 +13,10 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -53,7 +58,15 @@ class MainActivity : ComponentActivity() {
 
      @Composable
     fun RowScope.MyItem() {
-        Text(text = "Amr Nasser")
+        var counter by remember {
+            mutableStateOf(0)
+        }
+        Text(
+            text = "Counter = $counter",
+            modifier = Modifier.clickable {
+                counter++
+            }
+        )
     }
 
 }
