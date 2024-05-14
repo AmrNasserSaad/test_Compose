a9e41436642bbcd9f317aeb61b3a5d48fb8c0640
AmrNasser <amr931111@gmail.com>
2024-05-14 21:01:16 +0300

    test Box and Scopes

--- app/src/main/java/com/example/test_compose/MainActivity.kt
+++ app/src/main/java/com/example/test_compose/MainActivity.kt
@@ -4,60 +4,58 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.BoxScope
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.RowScope
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Column(
-                modifier = Modifier
 
+            BoxTest()
+        }
+
+    }
+
+
+    @Composable
+    fun BoxTest() {
+
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .background(Color.Cyan)
+
+        ) {
+
+            Row(
+                modifier = Modifier
+                    .background(Color.LightGray)
+                    .align(Alignment.Center)
+                    .padding(10.dp)
             ) {
 
-                Column(
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .background(Color.Red)
-
-                        .padding(20.dp)
-
-
-                ) {
-                    Text(text = "تحيا مصر ")
-                }
-                Column(
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .background(Color.White)
-                        .padding(20.dp)
-
-
-                ) {
-                    Text(text = "تحيا مصر ")
-
-                }
-                Column(
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .background(Color.Black)
-                        .padding(20.dp)
-
-
-                ) {
-                    Text(text = "تحيا مصر ", color = Color.White, textAlign = TextAlign.Center)
-                }
+                MyItem()
             }
 
         }
     }
+
+     @Composable
+    fun RowScope.MyItem() {
+        Text(text = "Amr Nasser")
+    }
+
 }
 
 
