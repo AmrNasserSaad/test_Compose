b69e360705b5dc4e4c670ec239622830dd87b1b5
AmrNasser <amr931111@gmail.com>
2024-05-15 18:16:20 +0300

    test @Preview

--- app/src/main/java/com/example/test_compose/composable/PreviewView.kt
+++ app/src/main/java/com/example/test_compose/composable/PreviewView.kt
@@ -1,12 +1,8 @@
-package com.example.test_compose
+package com.example.test_compose.composable
 
-import android.os.Bundle
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.BoxScope
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.RowScope
 import androidx.compose.foundation.layout.fillMaxSize
@@ -20,20 +16,42 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.example.test_compose.composable.BoxTest
 
-class MainActivity : ComponentActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContent {
+@Preview()
+@Composable
+fun BoxTest() {
 
-            BoxTest()
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color.Blue )
+
+    ) {
+
+        Row(
+            modifier = Modifier
+                .background(Color.LightGray)
+                .align(Alignment.Center)
+                .padding(10.dp)
+        ) {
+
+            MyItem()
         }
 
     }
-
-
 }
 
-
+@Composable
+fun RowScope.MyItem() {
+    var counter by remember {
+        mutableStateOf(0)
+    }
+    Text(
+        text = "Counter = $counter",
+        modifier = Modifier.clickable {
+            counter++
+        }
+    )
+}
