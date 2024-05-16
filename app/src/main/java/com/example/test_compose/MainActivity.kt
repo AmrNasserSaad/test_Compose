f684f153ba07a86f9de1a69cd709151b5c11ea6e
AmrNasser <amr931111@gmail.com>
2024-05-16 19:29:48 +0300

    test Conditions and loops

--- app/src/main/java/com/example/test_compose/MainActivity.kt
+++ app/src/main/java/com/example/test_compose/MainActivity.kt
@@ -4,11 +4,18 @@ import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.FloatingActionButton
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBar
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 
 class MainActivity : ComponentActivity() {
     @OptIn(ExperimentalMaterial3Api::class)
@@ -19,11 +26,68 @@ class MainActivity : ComponentActivity() {
 
             // BoxTest()
 
-           // FoodApp()
+            // FoodApp()
 
+            // testALotOfThing()
+
+           // testConitionAndLoops(team = listOf("Ahmed", "Ahmed", "Amr", "Ahmed", "Ahmed", "Ahmed"))
 
         }
     }
+
+//    @Composable
+//    private fun testConitionAndLoops(team: List<String>) {
+//
+//        Column(
+//            modifier = Modifier
+//                .fillMaxSize(),
+//            horizontalAlignment = Alignment.CenterHorizontally,
+//            verticalArrangement = Arrangement.SpaceEvenly
+//        ) {
+//            team.forEach {
+//                if (it == "Amr") {
+//                    Button(
+//                        modifier = Modifier.width(200.dp),
+//                        onClick = { /*TODO*/ }) {
+//                        Text(
+//                            text = "$it"
+//                        )
+//                    }
+//                } else {
+//                    Text(
+//                        text = "$it"
+//                    )
+//                }
+//
+//            }
+//        }
+//    }
+
+//    @Composable
+//    private fun testALotOfThing() {
+//        Column(
+//            modifier = Modifier
+//                .fillMaxSize(),
+//            horizontalAlignment = Alignment.CenterHorizontally,
+//            verticalArrangement = Arrangement.SpaceEvenly
+//        ) {
+//
+//            Text(
+//                modifier = Modifier
+//                    .background(color = Color.Cyan),
+//                fontSize = 66.sp,
+//                text = "Amr Naser"
+//            )
+//
+//            Button(
+//                modifier = Modifier.width(200.dp),
+//                onClick = { /*TODO*/ }) {
+//                Text(
+//                    text = "Click me"
+//                )
+//            }
+//        }
+//    }
 
 }
 
